bc58c88eb4098531132c005ead79a5b089bedad2
trangHa1999 <hatrang061299@yahoo.com>
2021-04-11 15:29:48 -0400

    Initial commit

--- bulletinBoard/settings.py
+++ bulletinBoard/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'jqbauqik6%6gw8q77&l-@jul92jn^gc$(@s=1gzc*rkglwo@a4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fir-django-a355c-pepyysgq7a-uc.a.run.app"]
+ALLOWED_HOSTS = ["34.123.76.28"]
 
 
 # Application definition
